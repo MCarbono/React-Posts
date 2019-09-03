a962361bed323dc8824a21379f1fd70003103011
MCarbono <carbono.marcelo@gmail.com>
2019-09-03 16:11:05 -0300

    first commit

---
+++ src/components/Posts.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import '../App.css';
+import Post from './Post.jsx';
+import dataJson from '../posts.json';
+
+class Posts extends React.Component {
+    constructor(props){
+        super(props);
+
+    this.state = {posts:dataJson.posts}
+    }
+
+    _removePost = (id) =>{
+        var posts = this.state.posts.filter((element) => {return element.id != id})
+        this.setState({posts});
+    }
+
+    _likePost = (id) =>{
+        var posts = this.state.posts.map((element) => {
+
+            if(element.id == id)
+                element.like = !element.like;
+
+            return element;
+        })
+
+        this.setState(posts);
+    }
+
+    render(){
+        return (
+            <div className="App">
+                 {this.state.posts.map((post, key) => 
+                    <Post key={key} id={post.id} like={post.like} _likePost={this._likePost} title={post.title} url={post.url} _removePost={this._removePost}/>
+                 )}
+            </div>
+        )
+    }
+
+}
+
+export default Posts
---
+++ src/screens/posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../App.css';
+import Posts from '../components/Posts.jsx';
+
+class PostsScreeen extends React.Component {
+    render(){
+        return(<Posts/>)
+    }
+}
+
+export default PostsScreeen;
